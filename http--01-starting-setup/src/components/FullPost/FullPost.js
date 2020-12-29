4c9a26f9fe9c2181a19079fae2fe1f0bed50f206
Darian Low <dlow3@jhu.edu>
2020-12-29 10:44:02 -1000

    delete of node modules

--- http--01-starting-setup/src/components/FullPost/FullPost.js
+++ http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -9,13 +9,20 @@ class FullPost extends Component {
     componentDidUpdate() {
         if (this.props.id) {
             if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
-                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.id)
                     .then(response => {
                         //console.log(response);
                         this.setState({loadedPost: response.data})
                     })
             }
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('/posts/' + this.state.id )
+            .then(response => {
+                console.log(response);
+            })
     }
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
@@ -26,9 +33,9 @@ class FullPost extends Component {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.content}</p>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
 
--- http--01-starting-setup/src/components/NewPost/NewPost.js
+++ http--01-starting-setup/src/components/NewPost/NewPost.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Darian'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        axios.post('/posts', data)
+            .then(response => {
+                console.log(response);
+            })
     }
 
     render () {
@@ -22,7 +34,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
